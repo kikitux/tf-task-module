3e9fe1c9b4b2feac9dbc59208b065553e1f6ff66
Alvaro Miranda Aguilera <kikitux@gmail.com>
2019-05-03 10:36:03 +0200

    meh

--- example/main.tf
+++ example/main.tf
@@ -1,3 +1,6 @@
+# we can use the module from github
+# per https://www.terraform.io/docs/modules/sources.html#github
+
 module "mymodule" {
-  source = "../"
+  source = "github.com/kikitux/tf-task-module"
 }
